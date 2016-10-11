338db9667dd34d9771fb27ba7340c43e2727655d
sxjiang <273893558@qq.com>
2016-10-11 12:03:43 +0800

    add ApplicationMain

---
+++ service/src/main/java/com/handpay/arch/stat/ics/Application.java
@@ -0,0 +1,12 @@
+package com.handpay.arch.stat.ics;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by sxjiang on 16/10/11.
+ */
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
