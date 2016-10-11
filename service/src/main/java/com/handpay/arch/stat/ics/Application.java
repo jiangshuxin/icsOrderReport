bccba3d94885833635db59fb168955cf64819d6c
zhengfc <zhengfc323@gmail.com>
2016-10-11 12:46:16 +0800

    update pom

--- service/src/main/java/com/handpay/arch/stat/ics/Application.java
+++ service/src/main/java/com/handpay/arch/stat/ics/Application.java
@@ -1,12 +1,23 @@
 package com.handpay.arch.stat.ics;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by sxjiang on 16/10/11.
  */
+@RestController
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args){
-        SpringApplication.run(Application.class,args);
-    }
+	
+	@RequestMapping("/hello")
+	public String home() {
+		return "World!";
+	}
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(Application.class, args);
+	}
 }
