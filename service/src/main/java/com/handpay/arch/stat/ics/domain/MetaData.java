ca947453d8374b7fb01b3fb2455a73a5187f9620
sxjiang <273893558@qq.com>
2016-10-12 09:29:53 +0800

    add config files

--- service/src/main/java/com/handpay/arch/stat/ics/domain/MetaData.java
+++ service/src/main/java/com/handpay/arch/stat/ics/domain/MetaData.java
@@ -2,7 +2,7 @@ package com.handpay.arch.stat.ics.domain;
 
 public class MetaData {
 	public enum StatType {
-		OnlineBatch(0), OfflineBatch(1), YangguangOnline(2), Mall(3);
+		OnlineBatch(0), OfflineBatch(1), SunshineOnline(2), Mall(3);
 		private StatType(int ordinal){}
 	}
 }
