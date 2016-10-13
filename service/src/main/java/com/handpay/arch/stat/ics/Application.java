7462573a2bdd86cca1f220deed5c85c01b25e19c
sxjiang <273893558@qq.com>
2016-10-13 15:40:37 +0800

    update service

--- service/src/main/java/com/handpay/arch/stat/ics/Application.java
+++ service/src/main/java/com/handpay/arch/stat/ics/Application.java
@@ -1,5 +1,8 @@
 package com.handpay.arch.stat.ics;
 
+import com.handpay.arch.stat.ics.domain.MetaData;
+import com.handpay.arch.stat.ics.domain.Stat;
+import com.handpay.arch.stat.ics.service.ReportService;
 import com.handpay.rache.core.spring.StringRedisTemplateX;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by sxjiang on 16/10/11.
@@ -18,10 +23,17 @@ public class Application {
 
 	@Autowired
 	private StringRedisTemplateX stringRedisTemplateX;
+	@Autowired
+	private ReportService reportService;
 	
 	@RequestMapping("/hello")
 	public String home() {
 		stringRedisTemplateX.keys("test");
+		for(MetaData.StatType type : MetaData.StatType.values()){
+			List<Stat> statList = reportService.embraceSumAndUndone(type);
+			reportService.makeSnapshot(statList.toArray(new Stat[0]));
+		}
+		reportService.exportExcel("20160601","20161012");
 		return "World!";
 	}
 	
